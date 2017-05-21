578d69aa27b52f7aed3d1ff793f1172882d5a063
David Taylor <daves@davids-imac.local>
2017-05-20 23:31:47 -0400

    Initial Commit

---
+++ Up/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Up
+//
+//  Created by David Taylor on 5/20/17.
+//  Copyright © 2017 Hyper Elephant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
